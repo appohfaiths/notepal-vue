7ef006bcdafe3d4ebca1a1637aac18ce0fc1fdf4
KodjoCode <appohfaiths@gmail.com>
2024-02-06 20:12:37 +0000

    feat: add function to pinia store to fetch notes

--- src/stores/notes.ts
+++ src/stores/notes.ts
@@ -3,10 +3,34 @@ import { defineStore } from 'pinia'
 
 export const useNotesStore = defineStore('notes', () => {
   const notes = ref([
-    { id: 1, text: 'Note 1' },
-    { id: 2, text: 'Note 2' },
-    { id: 3, text: 'Note 3' }
+    { note_id: 1, title: 'Note 1' },
+    { note_id: 2, title: 'Note 2' },
+    { note_id: 3, title: 'Note 3' }
   ])
 
-  return { notes }
+  const deleteNote = computed(() => {
+    return (id: number) => {
+      notes.value = notes.value.filter(note => note.note_id !== id)
+    }
+  })
+
+  async function getNotes() {
+    try {
+      // Fetch notes from an API
+      const response = await fetch('https://zfcq5cgd7np6zqyi3g5xint2yi0fvbbq.lambda-url.us-east-1.on.aws/list-notes/frontend')
+      const data = await response.json()
+      console.log(data.notes)
+      notes.value = data.notes
+      return notes.value
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  function $reset() {
+    notes.value = []
+  }
+
+  return { notes, getNotes, deleteNote, $reset }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,29 @@
 <script setup lang="ts">
 import { useNotesStore } from '@/stores/notes';
+import { storeToRefs } from 'pinia';
 
- const {notes} = useNotesStore();
+const store = useNotesStore();
+const { notes } = storeToRefs(store);
+const { $reset, getNotes } = store;
+// store.$patch({
+//   notes: [
+//     { id: 1, text: 'Learn Vue 3' },
+//     { id: 2, text: 'Learn Pinia' },
+//     { id: 3, text: 'Build a project' },
+//   ],
+// });
+
 </script>
 
 <template>
   <main>
     <h1>Home</h1>
     <ul>
-      <li v-for="note in notes" :key="note.id" >
-        {{ note.text }}
+      <li v-for="note in notes" :key="note.note_id" >
+        {{ note.title }}
       </li>
     </ul>
+    <button @click="getNotes">Get notes</button>
+    <button @click="$reset">Clear notes</button>
   </main>
 </template>
