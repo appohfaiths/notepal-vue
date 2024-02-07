fb441745c54ca825b283922a41dbd792120ee7af
KodjoCode <appohfaiths@gmail.com>
2024-02-07 02:04:25 +0000

    feat: create note type and add crud actions to store

--- src/stores/notes.ts
+++ src/stores/notes.ts
@@ -1,18 +1,13 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import type { Note } from '@/types'
 
 export const useNotesStore = defineStore('notes', () => {
   const notes = ref([
-    { note_id: 1, title: 'Note 1' },
-    { note_id: 2, title: 'Note 2' },
-    { note_id: 3, title: 'Note 3' }
+    { note_id: "1", title: 'Note 1' },
+    { note_id: "2", title: 'Note 2' },
+    { note_id: "3", title: 'Note 3' }
   ])
-
-  const deleteNote = computed(() => {
-    return (id: number) => {
-      notes.value = notes.value.filter(note => note.note_id !== id)
-    }
-  })
 
   async function getNotes() {
     try {
@@ -28,9 +23,64 @@ export const useNotesStore = defineStore('notes', () => {
     }
   }
 
+  async function addNote(note: Note) {
+    try {
+      const response = await fetch('https://zfcq5cgd7np6zqyi3g5xint2yi0fvbbq.lambda-url.us-east-1.on.aws/create-note', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+      })
+      const data = await response.json()
+      console.log(data)
+      return data
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function updateNote(note: Note) {
+    try {
+      const response = await fetch('https://zfcq5cgd7np6zqyi3g5xint2yi0fvbbq.lambda-url.us-east-1.on.aws/create-note', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+      })
+      const data = await response.json()
+      console.log(data)
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function deleteNote(note_id: string) {
+    try {
+        await fetch(`https://zfcq5cgd7np6zqyi3g5xint2yi0fvbbq.lambda-url.us-east-1.on.aws/delete-note/${note_id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(
+        response => {
+          console.log(response)
+          const data = response.json()
+          return data
+        }
+      );
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
   function $reset() {
     notes.value = []
   }
 
-  return { notes, getNotes, deleteNote, $reset }
+  return { notes, getNotes, addNote, updateNote, deleteNote, $reset }
 })
